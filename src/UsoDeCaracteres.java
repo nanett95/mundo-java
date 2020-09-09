c340cb6409a60e3d42c6c628f056d98aa5bf59be
Nanett Sandoval <nanettsandoval@hotmail.com>
2020-09-09 14:00:39 -0400

    Commit Inicial

---
+++ src/UsoDeCaracteres.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+//Mi clase en Java
+public class UsoDeCaracteres {
+
+    public static void main(String args[]) {
+       char miCaracter = 'a';
+        System.out.println("miCaracter = " + miCaracter);
+        
+        var varChar = '\u0021';
+        System.out.println("varChar1 = " + varChar);
+        
+        var varCharDecimal2 = (char)33;
+        System.out.println("varCharDecimal2 = " + varCharDecimal2);
+        
+        var varCharSimbolo3 = '!';
+        System.out.println("varCharSimbolo3 = " + varCharSimbolo3);
+        
+        int variableEnteraSimbolo = '!';
+        System.out.println("variableEnteraSimbolo = " + variableEnteraSimbolo);
+        
+        int letra = 'A';
+        System.out.println("letra = " + letra);
+    }
+}
+
+
