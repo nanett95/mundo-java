0ed3de60c6b01dcef92374eda84db20e52cb6899
Nanett Sandoval <nanettsandoval@hotmail.com>
2020-09-16 16:34:34 -0400

    Operadores Aritmeticos y tarea

---
+++ src/TareaOperadoresAritmeticos.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+
+public class TareaOperadoresAritmeticos {
+   public static void main (String args[]){
+       System.out.println("Alto: ");
+       Scanner consola = new Scanner(System.in);
+       int alto = Integer.parseInt(consola.nextLine());
+       System.out.println("usuario = " + alto);
+       
+       System.out.println("Ancho: ");
+       int ancho = Integer.parseInt(consola.nextLine());
+       System.out.println("usuario2 = " + ancho);
+       
+       
+       int area = alto * ancho;
+       System.out.println("area = " + area);
+       
+       int perimetro = (alto + ancho) * 2;
+       System.out.println("perimetro = " + perimetro);
+       
+       
+       
+       
+       
+       
+   }
+
+    
+}
